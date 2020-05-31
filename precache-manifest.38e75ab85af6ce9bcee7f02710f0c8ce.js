6669a36caac80325837bd73f546a0725f684ef15
Hridyaesh Sharma <dev.hridayesh@gmail.com>
2020-05-31 12:29:11 +0530

    Updates

---
+++ precache-manifest.38e75ab85af6ce9bcee7f02710f0c8ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "266c1b5918cc323aabc10c1b259c118e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "90a68ae2f0d35f186a96",
+    "url": "/monsters-rolodex/static/css/main.0606e802.chunk.css"
+  },
+  {
+    "revision": "bafbcdcd9a8666da3acb",
+    "url": "/monsters-rolodex/static/js/2.39f5c4ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.39f5c4ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90a68ae2f0d35f186a96",
+    "url": "/monsters-rolodex/static/js/main.d2d2cd99.chunk.js"
+  },
+  {
+    "revision": "062fc814831e2a83dc0d",
+    "url": "/monsters-rolodex/static/js/runtime-main.cbe33ecf.js"
+  }
+]);
